900abc33e1a6937d1edd87c07df9c6542c2ad8e2
jtwong2 <jtwong2@ncsu.edu>
2023-05-08 12:10:55 -0400

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-#!bin/bash
+#!/bin/bash
 
 # Install dependencies
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 sudo pip3 install -r requirements.txt
 
 # Launch bot
-python3 ticket-bot/bots/twitter_bot.py
+python3 bots/twitter_bot.py
