d9b900ab5c40f305c9fd1310d350d46184e542e7
jtwong2 <jtwong2@ncsu.edu>
2023-05-08 15:23:55 -0400

    More script fixes

--- local_setup.sh
+++ local_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Copy project to remote machine
-scp -r ./* $1:~/twitter-bot
+# SSH into remote machine and clone code
+ssh jtwong2@$1 'git clone https://github.com/jwong91/ticket-bot.git'
+
+# Copy secrets to remote machine
+scp -r ./secrets.json jtwong2@$1:~/ticket-bot/
 
 # SSH into remote machine and run setup script
-ssh $1 'bash -s' < ./remote_setup.sh
+cd /home/jtwong2/ticket-bot/
+ssh jtwong2@$1 'bash -s' < ./remote_setup.sh
--- remote_setup.sh
+++ remote_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Install dependencies
+cd /home/jtwong2/ticket-bot/
 sudo apt-get install -y python3-pip
 sudo pip3 install -r requirements.txt
 
 # Launch bot
-python3 bots/twitter_bot.py
+cd /home/jtwong2/ticket-bot/bots/
+pwd
+python3 twitter_bot.py
